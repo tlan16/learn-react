59a1f2fb69bc9666d3bb8c004a61c06960cca99d
Frank Lan <franklan118@gmail.com>
2018-06-21 17:01:15 +1000

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': 'airbnb',
+  'rules': {
+    'semi': 'off',
+    'import/prefer-default-export': 'off',
+  },
+  'globals': {
+  },
+}
