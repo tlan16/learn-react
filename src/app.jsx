47e0cdadb8d753c65d068038c2fe83692685875a
Frank Lan <franklan118@gmail.com>
2018-06-23 14:01:07 +1000

    fix coding style and rename to jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const app = rootElementId =>{
-
+const app = rootElementId => {
   ReactDOM.render(
     <h1>Hello, world!</h1>,
-    document.getElementById(rootElementId)
+    document.getElementById(rootElementId),
   )
 }
 
