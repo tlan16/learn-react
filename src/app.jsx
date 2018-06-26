a568671e5e32c479038c0ec258936ed46fd360f8
Frank Lan <franklan118@gmail.com>
2018-06-26 12:34:16 +1000

    fix react init issue

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import store from './store'
 import TodoApp from './todo/index'
+import { REACT_ROOT_ELEMENT_ID } from './index'
 
-const render = rootElement => {
+
+const render = () => {
   ReactDOM.render(
     <TodoApp
       todos={store.getState().todos}
     />,
-    rootElement,
+    document.getElementById(REACT_ROOT_ELEMENT_ID),
     () => store.subscribe(render),
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import App from './app'
 
+export const REACT_ROOT_ELEMENT_ID = 'react-root'
+
 const reactRoot = document.createElement('div')
-reactRoot.id = 'react-root'
+reactRoot.id = REACT_ROOT_ELEMENT_ID
 
 document.body.appendChild(reactRoot)
 
-App(reactRoot)
+App()
