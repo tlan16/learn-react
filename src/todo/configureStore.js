dc48d83e9711b931b499ea14977bef8342d013c5
Frank Lan <franklan118@gmail.com>
2018-07-03 18:58:59 +1000

    use middlewares of promise and logger

--- src/todo/configureStore.js
+++ src/todo/configureStore.js
@@ -1,37 +1,36 @@
 /* eslint-disable no-console */
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import promise from 'redux-promise'
+import { createLogger } from 'redux-logger'
 import todoAppReducers from './reducers'
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch
-  if (!console.group) { return rawDispatch }
-
-  return (action) => {
-    console.group(action.type)
-    console.log('%c previous state', 'color: grey', store.getState())
-    console.log('%c action', 'color: blue', action)
-    const returnValue = rawDispatch(action)
-    console.log('%c next state', 'color: green', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
-  }
-}
-
-const configureStore = () => {
-  const store = createStore(
-    todoAppReducers,
-    undefined,
-    process.env.NODE_ENV !== 'production'
-      // eslint-disable-next-line no-underscore-dangle
-      ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      : undefined,
-  )
-
+const getMiddlewares = () => {
+  const middlewares = [promise]
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store)
+    middlewares.push(createLogger())
   }
 
-  return store
+  return middlewares
 }
+
+const getEnhancer = (middlewares) => {
+  /* eslint-disable no-underscore-dangle */
+  const composeEnhancers =
+    process.env.NODE_ENV !== 'production'
+    && typeof window === 'object'
+    && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+      : compose;
+  /* eslint-enable */
+
+  return composeEnhancers(
+    applyMiddleware(...middlewares),
+  )
+}
+
+const configureStore = () => createStore(
+  todoAppReducers,
+  getEnhancer(getMiddlewares()),
+)
 
 export default configureStore
