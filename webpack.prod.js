8bea32f6ce768b75efa8e249ec910fa821bf938d
Frank Lan <frank.lan@balanceinternet.com.au>
2019-09-04 18:02:02 +1000

    use `terser` as js minimizer as recommended in react official doc

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,9 @@
 const merge = require('webpack-merge')
+const TerserPlugin = require('terser-webpack-plugin')
 const common = require('./webpack.common.js')
 
 const optimization = {
+  minimizer: [new TerserPlugin()],
   splitChunks: {
     chunks: 'all',
   },
